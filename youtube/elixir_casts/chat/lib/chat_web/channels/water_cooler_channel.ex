9107a62a3c87c9d07af05900b3cf9a76aeece973
Jim Park <jimp@scribd.com>
2020-09-24 20:26:24 -0700

    feat(chats) add Chats model to store messages

--- youtube/elixir_casts/chat/lib/chat_web/channels/water_cooler_channel.ex
+++ youtube/elixir_casts/chat/lib/chat_web/channels/water_cooler_channel.ex
@@ -1,6 +1,7 @@
 defmodule ChatWeb.WaterCoolerChannel do
   use ChatWeb, :channel
-
+  
+  alias Chat.Chats
   @impl true
   def join("water_cooler:lobby", _payload, socket) do
       {:ok, socket}
@@ -10,6 +11,7 @@ defmodule ChatWeb.WaterCoolerChannel do
   # broadcast to everyone in the current topic (water_cooler:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Chats.create_message(payload)
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
