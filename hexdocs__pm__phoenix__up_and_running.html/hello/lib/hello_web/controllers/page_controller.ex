be97b6760a61f64183183d57a07f99e293b0836c
Jim Park <jimp@scribd.com>
2020-09-25 21:42:08 -0700

    feat(hello) Add some different mime.types

--- hexdocs__pm__phoenix__up_and_running.html/hello/lib/hello_web/controllers/page_controller.ex
+++ hexdocs__pm__phoenix__up_and_running.html/hello/lib/hello_web/controllers/page_controller.ex
@@ -1,8 +1,18 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+
+  def index(conn, %{"_format" => "json"}) do
+    json(conn, %{index: "asdf"})
+  end
+
+  def index(conn, %{"just_status" => "true"}) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(201, "")
+  end
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, :index)
   end
 
   def index_admin(conn, _params) do
