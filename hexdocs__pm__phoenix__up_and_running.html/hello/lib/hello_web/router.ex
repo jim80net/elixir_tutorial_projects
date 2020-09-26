9091d4f0fa6ca2b2b511240938900f3c3ea754e6
Jim Park <jimp@scribd.com>
2020-09-25 20:45:40 -0700

    Revert "feat(hello): Add some (empty) routes and pipelines"
    This reverts commit 5c07e654eced7759f2c1894d5a4e1ff346e4f338.

--- hexdocs__pm__phoenix__up_and_running.html/hello/lib/hello_web/router.ex
+++ hexdocs__pm__phoenix__up_and_running.html/hello/lib/hello_web/router.ex
@@ -14,46 +14,12 @@ defmodule HelloWeb.Router do
     plug :accepts, ["json"]
   end
 
-  pipeline :review_checks do
-    plug :ensure_authenticated_user
-    plug :ensure_user_owns_review
-  end
-
   scope "/", HelloWeb do
     pipe_through :browser
-
-    resources "/users", UserController do
-      resources "/posts", PostController, only: [:index, :show] do
-        resources "/comments", CommentController, except: [:delete]
-      end
-      scope "/reviews", ReviewController do
-        pipe_through :review_checks
-
-        resources "/reviews", ReviewController   
-      end
-    end
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
-  end
-
-  scope "/admin", HelloWeb.Admin, as: :admin do
-    pipe_through :browser
-
-    resources "/reviews", ReviewController
-    resources "/images",  ImageController
-    resources "/users",   UserController
-  end
-
-  scope "/api", HelloWeb.Api, as: :api do
-    pipe_through :api
-  
-    scope "/v1", V1, as: :v1 do
-      resources "/images",  ImageController
-      resources "/reviews", ReviewController
-      resources "/users",   UserController
-    end
   end
 
   # Other scopes may use custom stacks.
