5d91b47e67cc2690e8978ba68fe1f06678be3585
Jim Park <jimp@scribd.com>
2020-09-28 01:13:38 -0700

    feat(hello): Add hello world Mix Task
    https://hexdocs.pm/phoenix/mix_tasks.html

---
+++ hexdocs__pm__phoenix__up_and_running.html/hello/lib/mix/tasks/hello.greeting.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.Hello.Greeting do
+  use Mix.Task
+
+  @shortdoc "Sends a greeting to us from Hello Phoenix"
+
+  @moduledoc """
+  This is where we would put longform documentation or doc tests
+  """
+
+  def run(args) do
+    Mix.Task.run("app.start")
+    Mix.shell().info("Greetings from the Hello Phoenix Application #{IO.inspect args}")
+  end
+end
