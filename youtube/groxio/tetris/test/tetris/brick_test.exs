db081056bd61eba5bc4383eb2396f0cdfa2a1fb6
Jim Park <jimp@scribd.com>
2020-10-11 16:42:50 -0700

    Add brick movement
    https://www.youtube.com/watch?v=FPIel4x1hcc

--- youtube/groxio/tetris/test/tetris/brick_test.exs
+++ youtube/groxio/tetris/test/tetris/brick_test.exs
@@ -9,8 +9,22 @@ defmodule BrickTest do
   test "Creates a new random brick" do
     actual = new_random()
 
-    assert actual.name in ~w( i l z t o)a
+    assert actual.name in ~w( i l z t o )a
     assert actual.rotation in [0, 90, 180, 270]
     assert actual.reflection in [true, false]
+  end
+
+  test "should manipulate brick" do
+    actual =
+      new()
+      |> left
+      |> right
+      |> right
+      |> down
+      |> spin_90
+      |> spin_90
+
+    assert actual.location == {41, 1}
+    assert actual.rotation == 180
   end
 end
