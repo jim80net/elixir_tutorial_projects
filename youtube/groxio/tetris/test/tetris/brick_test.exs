81cf5f79806a57478a99fc7dfb68ab75899c3311
Jim Park <jimp@scribd.com>
2020-10-11 16:24:39 -0700

    test for new_random
    https://www.youtube.com/watch?v=FPIel4x1hcc

--- youtube/groxio/tetris/test/tetris/brick_test.exs
+++ youtube/groxio/tetris/test/tetris/brick_test.exs
@@ -5,4 +5,12 @@ defmodule BrickTest do
   test "Creates a new brick" do
     assert new().name == :i
   end
+
+  test "Creates a new random brick" do
+    actual = new_random()
+
+    assert actual.name in ~w( i l z t o)a
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
+  end
 end
