da7a557a1eb497998c5688d403cb9cd706d74ae2
Jim Park <jimp@scribd.com>
2020-10-11 17:48:36 -0700

    Add rotation functions to points
    https://www.youtube.com/watch?v=se1q0DNF3P8&list=PLKBMoE8mCkXj9KTMNWYgvTFRyahT6PF2q&index=4

--- youtube/groxio/tetris/lib/tetris/points.ex
+++ youtube/groxio/tetris/lib/tetris/points.ex
@@ -2,4 +2,49 @@ defmodule Tetris.Points do
   def translate(points, {dx, dy}) do
     Enum.map(points, fn {x, y} -> {dx + x, dy + y} end)
   end
+
+  def transpose(points) do
+    points
+    |> Enum.map(fn {x, y} -> {y, x} end)
+  end
+
+  def mirror(points) do
+    points
+    |> Enum.map(fn {x, y} -> {4 - x, y} end)
+  end
+
+  def flip(points) do
+    points
+    |> Enum.map(fn {x, y} -> {x, 4 - y} end)
+  end
+
+  # x
+  # x
+  # x x
+  #  -- -- --> transpose
+  # x x x
+  #     x
+  # -- -- --> mirror
+  # x x x
+  # x   
+  def rotate_90(points) do
+    points
+    |> transpose
+    |> mirror
+  end
+
+  def rotate(points, 0), do: points
+  def rotate(points, 90), do: rotate_90(points)
+
+  def rotate(points, 180) do
+    points
+    |> rotate(90)
+    |> rotate(90)
+  end
+
+  def rotate(points, 270) do
+    points
+    |> rotate(90)
+    |> rotate(180)
+  end
 end
