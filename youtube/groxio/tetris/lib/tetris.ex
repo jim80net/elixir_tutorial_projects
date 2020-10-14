8f61115be31f2f6c055204b7038eac943a464690
Jim Park <jimp@scribd.com>
2020-10-13 23:46:00 -0700

    [tetris] mix phx.new tetris --no-ecto
    https://www.youtube.com/watch?v=g57jmNG4SqQ&list=PLKBMoE8mCkXj9KTMNWYgvTFRyahT6PF2q&index=8

--- youtube/groxio/tetris/lib/tetris.ex
+++ youtube/groxio/tetris/lib/tetris.ex
@@ -1,18 +1,11 @@
 defmodule Tetris do
   @moduledoc """
-  Documentation for `Tetris`.
+  Tetris keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tetris.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def hello(), do: :world
 end
